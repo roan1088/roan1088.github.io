e191705a4e06414f7fb14a17678a515f81c6afa8
roan1088 <rohan_188@yahoo.com>
2020-12-04 17:19:04 -0500

    Added content to contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -2,8 +2,39 @@ import React from "react";
 import MainHeading from "../../components/MainHeading";
 
 function Contact() {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <MainHeading>Contact Me</MainHeading>
+    <>
+      <MainHeading>Contact Me</MainHeading>
+      
+      <div className="row">
+        <div className="col">
+          <form onSubmit={handleFormSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input className="form-control" id="name" placeholder="Name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" className="form-control" id="name" placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea className="form-control" id="message" rows="3"  placeholder="Message"></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+      <div className="row pt-4">
+        <div className="col">
+          <i className="fas fa-phone"></i> (347)449-0126 | <i className="fas fa-envelope"></i> rohan_188@yahoo.com
+        </div>
+      </div>
+    </>
   );
 }
 
