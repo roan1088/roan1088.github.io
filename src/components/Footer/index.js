671ab77d1913209d872e69281425ef239c9601a8
roan1088 <rohan_188@yahoo.com>
2020-12-04 00:39:52 -0500

    Changed class to className

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer class="fixed-bottom text-center">
-      <a href="https://github.com/roan1088" target="_blank" rel="noreferrer"><i class="fab fa-github fa-lg"></i></a>
-      <a href="https://www.linkedin.com/in/rohan-chitrakar-bba4791b5/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin fa-lg"></i></a>
+    <footer className="fixed-bottom text-center">
+      <a href="https://github.com/roan1088" target="_blank" rel="noreferrer"><i className="fab fa-github fa-lg"></i></a>
+      <a href="https://www.linkedin.com/in/rohan-chitrakar-bba4791b5/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-lg"></i></a>
     </footer>
   );
 }
