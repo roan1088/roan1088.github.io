50b8f9052406bd0a602217792cc1fb71fefb7273
roan1088 <rohan_188@yahoo.com>
2020-12-04 00:29:31 -0500

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import PageNotFound from './pages/PageNotFound';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/contact" component={Contact} />
           <Route component={PageNotFound} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer class="fixed-bottom text-center">
+      <a href="https://github.com/roan1088" target="_blank" rel="noreferrer"><i class="fab fa-github fa-lg"></i></a>
+      <a href="https://www.linkedin.com/in/rohan-chitrakar-bba4791b5/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin fa-lg"></i></a>
+    </footer>
+  );
+}
+
+export default Footer;
