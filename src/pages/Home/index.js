68d4c0a5a8806af6a1dbcea8407fa66061e9a4cc
roan1088 <rohan_188@yahoo.com>
2021-01-16 21:36:26 -0500

    Update about me

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,8 +12,7 @@ function Home() {
       <div className="row">
         <div className="col">
           <img className="float-left mr-4" src="https://avatars0.githubusercontent.com/u/71278881?v=4" alt="Rohan" width="150px" height="150px" />
-          <p>Hello World! My name is Rohan Chitrakar. I live in NYC, New York but I am originally from Lalitpur, Nepal. I came to the US in 2012 as a student. Right now I am taking this Coding bootcamp with Columbia University to get into computer science field and kickstart my career.</p>
-          <p>My favorite hobby is playing video games. My dream job is to become a game developer. I hope that from this bootcamp I am able to start my career in development which would a step towards my dream job.</p>
+          <p>Full stack web developer recognized for the ability to write clean and efficient code for mobile responsive web development. Complex problem solver with a certificate in full stack web development from Columbia University. Proven leader with effective team collaboration skills excited to leverage knowledge to  drive engaging user experiences.</p>
         </div>
       </div>
     </>
