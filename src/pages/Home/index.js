f54486d7344055f2830bd248e7088d2eda6da70d
roan1088 <rohan_188@yahoo.com>
2025-03-02 14:24:31 -0500

    Changed profile picture

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import MainHeading from "../../components/MainHeading";
 import Skill from "../../components/Skill";
+import proPic from "../../images/rohan-pro-pic.jpg";
 
 function Home() {
   useEffect(() => {
@@ -12,7 +13,7 @@ function Home() {
       <MainHeading>About Me</MainHeading>
       <div className="row">
         <div className="col">
-          <img className="float-left mr-4" src="https://avatars0.githubusercontent.com/u/71278881?v=4" alt="Rohan" width="150px" height="150px" />
+          <img className="float-left mr-4" src={proPic} alt="Rohan" width="150px" height="150px" />
           <p>Full stack web developer recognized for the ability to write clean and efficient code for mobile responsive web development. Complex problem solver with a certificate in full stack web development from Columbia University. Proven leader with effective team collaboration skills excited to leverage knowledge to  drive engaging user experiences.</p>
         </div>
       </div>
